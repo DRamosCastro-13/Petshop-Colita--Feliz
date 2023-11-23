6201ef494b261bbbe24de9e1dac5c1b9c8c0b9d4
Daniela Ramos <danirc01@gmail.com>
2023-11-23 12:25:04 -0500

    archivos iniciales de home, con assets

---
+++ assets/tailwind.config.extension.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const theme = {
+    extend: {
+        colors: {
+            'blue': '#083D77',
+            'beige' : '#EBEBD3',
+            'yellow' : '#F4D35E',
+            'orange' : '#EE964B',
+            'red' : '#F95738'
+        }
+    }
+};
+export const plugins = [];
---
+++ assets/tailwind.config.js
@@ -0,0 +1,14 @@
+export const plugins = []
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'blue': '#083D77',
+                'beige' : '#EBEBD3',
+                'yellow' : '#F4D35E',
+                'orange' : '#EE964B',
+                'red' : '#F95738'
+            }
+        }
+    }
+}
